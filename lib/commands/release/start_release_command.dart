f2f2fbc783fa0b80ec4b230e0ed3d9c8acedb6f9
ChessMax <chessmax@xakep.ru>
2020-07-31 17:20:43 +0300

    - simple code to retrieve data from browser

--- lib/commands/release/start_release_command.dart
+++ lib/commands/release/start_release_command.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+
 import 'package:alex/runner/alex_command.dart';
 
 /// Команда запуска релизной сборки.
@@ -17,7 +19,11 @@ class StartReleaseCommand extends AlexCommand {
     await _delay();
     print('completed');
     print('Waiting for change log...');
-    runBrowser('http://ya.ru');
+
+    final changeLog = await getChangeLog();
+
+    print('Change log: ' + changeLog);
+
     await _delay(15);
     print('completed');
     print('Finishing release branch...');
@@ -54,5 +60,63 @@ class StartReleaseCommand extends AlexCommand {
         fail = true;
         break;
     }
+  }
+
+  Future<String> getChangeLog() async {
+    final port = 4024;
+    final host = 'http://localhost:$port';
+
+    final data = getRawChangeLog(host, port);
+
+    runBrowser(host);
+
+    return await data;
+  }
+
+  Future<String> getRawChangeLog(String host, int port) async {
+    final server = await HttpServer.bind(InternetAddress.loopbackIPv4, port);
+    print('Listening on $host');
+
+    final completer = Completer<String>();
+
+    final changeLogTemplatePath = 'lib/assets/commands/release/change_log.html';
+    final file = File(changeLogTemplatePath);
+    final changeLogTemplate = await file.readAsString();
+
+//    final uri = await Isolate.resolvePackageUri(
+//        Uri(path: 'package:alex/commands/release/templates/change_log.html'));
+//
+//    print('cd: ' + Directory.current.toString());
+//
+//    print('Uri: $uri');
+//    print('Uri: ${uri.toFilePath()}');
+
+    var text = changeLogTemplate.replaceAll('%action%', host);
+
+    await for (HttpRequest request in server) {
+      try {
+        final response = request.response;
+
+        print("Request [${request.uri.toString()}]");
+
+        var changeLog = request.uri.queryParameters['changelog'];
+
+        if (changeLog != null && changeLog.isNotEmpty) {
+          completer.complete(changeLog);
+          response.writeln("Succeed");
+          await response.close();
+          break;
+        } else {
+          response.headers.contentType = ContentType.html;
+          response.statusCode = HttpStatus.ok;
+          response.writeln(text);
+          await response.close();
+        }
+      } catch (e) {
+        print('Handle request error: $e');
+      }
+    }
+
+    return completer.future;
   }
 }
