d9bc7f753f17f6c5e7215f690f441bcf1747e41a
greymag <greymag@gmail.com>
2020-08-14 14:56:22 +0300

    Add methods for print info messages and errors. Also add support for the verbose flog.

--- lib/runner/alex_command.dart
+++ lib/runner/alex_command.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
+import 'package:meta/meta.dart';
 
 /// Базовый класс команды.
 abstract class AlexCommand extends Command<int> {
@@ -7,7 +10,7 @@ abstract class AlexCommand extends Command<int> {
   final String _description;
   final ArgParser _argParser = ArgParser(
     allowTrailingOptions: false,
-  );
+  )..addFlag('verbose', help: 'Show additional diagnostic info');
 
   AlexCommand(this._name, this._description);
 
@@ -19,4 +22,18 @@ abstract class AlexCommand extends Command<int> {
 
   @override
   String get description => _description;
+
+  /// Prints message if verbose flag is on.
+  @protected
+  void printVerbose(String message) {
+    if (argResults['verbose'] as bool) print(message);
+  }
+
+  /// Prints some info message in output.
+  @protected
+  void printInfo(String message) => print(message);
+
+  /// Prints error message in error output.
+  @protected
+  void printError(String message) => stderr.writeln(message);
 }
